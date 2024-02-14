b47d31db029aba8d2c1d57b5aaf1099232a09ab9
ademijakshylykova <jakshylykova04@gmail.com>
2024-02-14 13:33:18 +0600

    matr

--- Main.java
+++ Main.java
@@ -5,10 +5,11 @@
 Write your code in this editor and press "Run" button to execute it.
 
 *******************************************************************************/
-
+// import java.util;
 public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
 	}
 }
